55ebd12b953445a71cbc5e8fa7585b791c59ab25
pedrokorwa <korwapedro@gmail.com>
2020-02-13 23:37:38 +0700

    jam 23:37

--- js/script.js
+++ js/script.js
@@ -50,7 +50,12 @@ $(window).scroll(function(){
 	}
 
 });
-
+new Typed('#typed',{
+  strings : ['HTML','CSS','BOOTSTRAP','JAVASCRIPT'],
+  typeSpeed : 100,
+  delaySpeed : 100,
+  loop : true
+});
 
 
 
